0574ece7bebb52ebbcd48765d113413e8c57859e
mfcaicedo <mfcaicedo@unicauca.edu.co>
2022-05-21 01:39:20 -0500

    version 1.3

--- index.py
+++ index.py
@@ -1,40 +1,57 @@
 
-from flask import Flask, jsonify, render_template, request
-# from flask_mysqldb  import MySQL
+from ast import Try
+from flask import Blueprint, Flask, flash, jsonify, render_template, request
 
 from flask_sqlalchemy import SQLAlchemy
+from models.evento import Evento
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/dbeventos"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-class Evento(db.Model):
-    eve_id = db.Column(db.Integer, primary_key=True)
-    eve_codigo = db.Column(db.Integer,nullable=False)
-    eve_nombre = db.Column(db.String(80), nullable=False)
-    eve_entradas = db.Column(db.Integer, nullable=False)
-    eve_fecha = db.Column(db.DateTime, nullable=False)
-    
-#Inserción en la base de datos, paso valores
-db.session.add(Evento(eve_codigo=10, eve_nombre='Exposicion', eve_entradas=100, eve_fecha='23-05-22'))
-db.session.commit() #confirmo inserción 
-evento = Evento.query.all() #inserta en la base de datos 
+with app.app_context():
+    db.create_all()
+
+eventos = Blueprint('eventos', __name__)
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    
+    #consulta base de datos 
+    eventos = Evento.query.all()
+    
+    
+    return render_template('index.html', eventos=eventos)
 
 def query_string():
     print(request)
-
-@app.route('/eventos')
-def eventos():
-    data = {}
+    
+@app.route("/formulario")
+def formulario():
+    return render_template('registrar.html')
+@app.route('/insertar', methods=['POST'])
+def insertar():
+    try:
+        datos = request.form
+        #Creo objeto Evento 
+        objEvento = Evento(datos['codigo'], datos['nombre'], datos['entradas'], datos['fecha'])
+        #Inserto objeto de evento a la sesion, (paso valores)
+        db.session.add(objEvento)
+        db.session.commit() #confirmo inserción 
+        # evento = objEvento.query.all() #guardo en la db 
+        
+        return "correcto" 
+    except Exception as ex:
+        return "error" 
 
 @app.route('/busqueda')
 def busqueda():
     return render_template('busqueda.html')
 
+@app.route('/reservar')
+def reservar():
+    return render_template('reservar.html')
+
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=5000)
--- models/evento.py
+++ models/evento.py
@@ -1,3 +1,15 @@
+from utils.db import db
 
-
-class Evento():
+class Evento(db.Model):
+    eve_id = db.Column(db.Integer, primary_key=True)
+    eve_codigo = db.Column(db.Integer,nullable=False)
+    eve_nombre = db.Column(db.String(80), nullable=False)
+    eve_entradas = db.Column(db.Integer, nullable=False)
+    eve_fecha = db.Column(db.DateTime, nullable=False)
+    
+    def __init__(self, eve_codigo, eve_nombre, eve_entradas, eve_fecha):
+        # self.eve_id = eve_id
+        self.eve_codigo = eve_codigo
+        self.eve_nombre = eve_nombre
+        self.eve_entradas = eve_entradas
+        self.eve_fecha = eve_fecha
